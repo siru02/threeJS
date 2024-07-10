a9a6852fe95f12e0e27b80ab7e63093db10b87a6
siru02 <khm32323@gmail.com>
2024-07-11 00:02:21 +0900

    정육면체 완성

--- three.js-master/study/01-basic.js
+++ three.js-master/study/01-basic.js
@@ -18,7 +18,8 @@ class App {
 		//WebGL객체가 생성되면 내부적으로 canvas를 생성함
 		console.dir(renderer); //this._renderer와 같다
 
-		const scene = new THREE.Scene();
+		const scene = new THREE.Scene();//scene객체를 생성한다
+		//scene객체는 THREE안의 생성자를 가져와서 호출
 		this._scene = scene;
 
 		this._setupCamera(); //카메라 객체 설정
@@ -26,11 +27,78 @@ class App {
 		this._setupModel(); //3차원 모델을 설정
 
 		window.onresize = this.resize.bind(this);
-		this.resize();
+		//화면의 크기가 resize되는 이벤트에서 객체의 resize되는 함수를 바인드
+		//bind로 넘겨주는 이유는
+		this.resize(); //현재창크기에 맞게 카메라, 광원, 렌더러를 설정
 
 		requestAnimationFrame(this.render.bind(this));
+		//3차원 그래픽을 만들어주는 메소드
+		//bind로 넘겨주는 이유는 render method의 코드 안에서 사용되는 this가
+		//app클래스의 객체를 가르키게 하기 위해서
+	}
+
+	_setupCamera() {
+		const width = this._divContainer.clientWidth;//3차원 가로
+		const heigth = this._divContainer.clientWidth;//3차원 세로
+		const camera = new THREE.PerspectiveCamera(
+			75,
+			width / heigth,
+			0.1,
+			100
+		); //카메라 객체를 생성
+		camera.position.z = 2;
+		this._camera = camera; //APP클래스 내의 변수로 지정해줌
+	}
+
+	_setupLight(){
+		const color = 0xffffff; //광원의 색상
+		const intensity = 1; //광원의 세기
+		const light = new THREE.DirectionalLight(color, intensity);
+		light.position.set(-1, 2, 4); //광원의 방향
+		this._scene.add(light);
+	}
+
+	_setupModel() { //모델을 정의하는 메소드이며 이번에 만들것은 정육면체
+		const geometry = new THREE.BoxGeometry(1, 1, 1);
+		//가로 세로 깊이에 대한 값을 받음
+		const material = new THREE.MeshPhongMaterial({color: 0x44a88});
+		//재질의 색을 설정
+		//three에서는 다양한 geometry와 material제공 이걸 공부해야할듯
+
+		const cube = new THREE.Mesh(geometry, material);//실제 객체 생성
+
+		this._scene.add(cube); //화면의 구성요소로 추가된다
+		this._cube = cube;
+	}
+
+	resize(){
+		const width = this._divContainer.clientWidth;
+		const heigth = this._divContainer.clientHeight;
+		//html의 div쪽의 크기를 가져와서 카메라의 속성값을 설정해줌
+
+		this._camera.aspect = width / heigth; //카메라 속성값 설저ㅇ
+		this._camera.updateProjectionMatrix();
+
+		this._renderer.setSize(width, heigth);
+	}
+
+	render(time) { //렌더링이 시작된 이후 경과된 밀리초를 받는다
+		//렌더러가 scenen을 카메라의 시점을 기준으로 렌더링하는작업을 한다
+		this._renderer.render(this._scene, this._camera);
+		this.update(time); //시간에 따라 애니메이션 효과를 발생시킨다
+		requestAnimationFrame(this.render.bind(this));
+		//생성자의 코드와 동일: 계속 렌더 메소드가 무한히 반복되어 호출되도록 만든다
+	}
+
+	update(time) {
+		time *= 0.001; //ms를 s로 바꿔준다
+
+		//x, y의 회전값에 시간값을 넣으면 x,y축으로 큐브가 회전하게된다
+		this._cube.rotation.x = time;
+		this._cube.rotation.y = time;
 	}
 }
+
 
 window.onload = function () {
 //window.onload는 웹 페이지가 모두 로드된 후 실행될 함수를 지정하는 이벤트 핸들러이다
